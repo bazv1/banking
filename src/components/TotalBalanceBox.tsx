49ea97d5e7841dbf96892354e2782140d9d821f5
bazv1 <steveafrikenya1@gmail.com>
2024-07-17 03:36:39 -0700

    new files

--- src/components/TotalBalanceBox.tsx
+++ src/components/TotalBalanceBox.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import { formatAmount } from '../../lib/utils'
 
 const TotalBalanceBox = ({
-    accounts = [], totalBanks, totalCurrentBalance  
+    // accounts = [],
+     totalBanks, totalCurrentBalance  
 }: TotlaBalanceBoxProps) =>{
     return (
         <section className="total-balance">
             <div className="total-balance-chart">
                 {/*DoughnutChart */}
+                
              </div>
 
              <div className="flex flex-col gap-6">
                 <h2 className="header-2">
                     Bank Accounts: {totalBanks}
+                    {/* {accounts} */}
                 </h2>
                 <div className="flex flex-col gap 2">
                  <p className="total-balance-label">
@@ -19,7 +23,7 @@ const TotalBalanceBox = ({
                   </p>
 
                 <p className="total-balance-amount flex-center gap-2">
-                   formatAmount(totalCurrentBalance)
+                {formatAmount(totalCurrentBalance)}
                 </p> 
              </div>
             </div>
