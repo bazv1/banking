26bcc11782100675dc290a40ba5090571fef93c6
bazv1 <steveafrikenya1@gmail.com>
2026-03-11 06:34:48 +0000

    Fix BankCard props typing

--- src/components/BankCard.tsx
+++ src/components/BankCard.tsx
@@ -1,22 +1,31 @@
-import { formatAmount } from '@/lib/utils'
-import Link from 'next/link'
-import React from 'react'
-import Image from 'next/image'
+import { formatAmount } from '@/lib/utils';
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
 
+interface BankCardProps {
+  account: {
+    name?: string;
+    currentBalance?: number;
+  };
+  userName: string;
+  showBalance?: boolean;
+}
 
-
-const BankCard = ({ account, userName, showBalance = true }:BankCard) => {
+const BankCard: React.FC<BankCardProps> = ({ account, userName, showBalance = true }) => {
   return (
-    <div className="flex lex-col">
+    <div className="flex flex-col">
       <Link href="/" className="bank-card">
         <div className="bank-card_content">
           <div>
             <h1 className="text-16 font-semibold text-white">
               {account.name || userName}
             </h1>
-            <p className="font-ibm-plex-serif font-black text-white">
-              {formatAmount(account.currentBalance)}
-            </p>
+            {showBalance && (
+              <p className="font-ibm-plex-serif font-black text-white">
+                {formatAmount(account.currentBalance ?? 0)}
+              </p>
+            )}
           </div>
 
           <article className="flex flex-col gap-2">
@@ -24,7 +33,7 @@ const BankCard = ({ account, userName, showBalance = true }:BankCard) => {
               <h1 className="text-12 font-semibold text-white">
                 {userName}
               </h1>
-              <h2 className="text-12 font-semibold text white">
+              <h2 className="text-12 font-semibold text-white">
                 ●● / ●●
               </h2>
             </div>
@@ -33,9 +42,10 @@ const BankCard = ({ account, userName, showBalance = true }:BankCard) => {
             </p>
           </article>
         </div>
+
         <div className="bank-card_icon">
           <Image
-            src="icons/Paypass.svg"
+            src="/icons/Paypass.svg"
             width={20}
             height={24}
             alt="pay"
@@ -57,10 +67,8 @@ const BankCard = ({ account, userName, showBalance = true }:BankCard) => {
           className="absolute top-0 left-0"
         />
       </Link>
-
-      {/* COPY */}
     </div>
-  )
-}
+  );
+};
 
-export default BankCard
+export default BankCard;
